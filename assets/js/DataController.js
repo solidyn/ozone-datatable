25d0377caaf28da94ac0cefa887467d80f028999
Chris Rothe <crothe@gmail.com>
2013-05-30 09:15:32 -0600

    finished color-by implementation for numbers

--- assets/js/DataController.js
+++ assets/js/DataController.js
@@ -3,7 +3,34 @@ var OWF = OWF || null;
 
 DATATABLEWIDGET.DataController = function () {
 	var dataTable = null,
-		displayedData;
+		displayedData,
+		colorBins = [
+			{
+				color: "#ff0000", // red
+				min: 0,
+				max: 10
+			},
+			{
+				color: "#00ff00", // green
+				min: 10,
+				max: 20
+			},
+			{
+				color: "#0000ff", // blue
+				min: 20,
+				max: 30
+			},
+			{
+				color: "#ffff00", // yellow
+				min: 30,
+				max: 40
+			},
+			{
+				color: "#00ffff", // cyan
+				min: 40,
+				max: 50
+			}
+		];
 
 	function initialize () {
 		OWF.Intents.receive(
@@ -64,8 +91,9 @@ DATATABLEWIDGET.DataController = function () {
 		});
 	}
 	
-    function initializeContextMenu() {
-		$('tbody').contextmenu({
+    function initializeContextMenu(data, associatedDataTableColumn) {
+
+		$('#datatable tbody').contextmenu({
 		    target: '#context-menu',
 		    onItem: function(e, item) {
 		    	if($(item).text() === "Color By") {
@@ -74,9 +102,58 @@ DATATABLEWIDGET.DataController = function () {
 		    }
 		 });
 
+		for (var i = 0; i < data.columns.length; i += 1) {
+			if (i !== associatedDataTableColumn - 1) {
+				var columnIndex = associatedDataTableColumn > -1 ? i + 1 : i;
+				$("#columns-selector").prepend("<label class='radio'><input column='" + columnIndex + "' type='radio' name='column-options' value='" + data.columns[i].title + "'>" + data.columns[i].title + "</label>")	
+			}
+		}
+		
+		for (var i = 0; i < colorBins.length; i += 1) {
+			$("#color-bins tbody").append("<tr> \
+				<td><div class='input-append color' data-color='" + colorBins[i].color + "' data-color-format='hex'> \
+				  <input id='color_" + i + "' type='text' class='span1' value='" + colorBins[i].color + "' ><span class='add-on'><i style='background-color: " + colorBins[i].color + ";'></i></span></div></td> \
+				<td><input type='text' id='min_" + i + "' value='" + colorBins[i].min + "'></td> \
+				<td><input type='text' id='max_" + i + "' value='" + colorBins[i].max + "'></td> \
+				</tr>"
+			);
+		}
+		$('.color').colorpicker();
+
 		$("#select-column-button").click(function() {
-			var selectedColumnValue = $('input[name=column-options]:checked').val();
+			var selectedColumn = $('input[name=column-options]:checked').val();
+			
+			for (var i = 0; i < colorBins.length; i += 1) {
+				colorBins[i].color = $("#color_" + i).val();
+				colorBins[i].min = parseFloat($("#min_" + i).val());
+				colorBins[i].max = parseFloat($("#max_" + i).val());
+			}
+			
+			appyColorBinsToColumn(selectedColumn);
 		});
+	}
+	
+	function appyColorBinsToColumn(columnTitle) {
+		var index = $("#datatable tr th:contains('" + columnTitle + "')").index();
+		if (index > -1) {
+			$("#datatable tbody").find('tr').each(function (){
+				var cell = $(this).children('td').get(index),
+				    color = findBinForValue(parseFloat($(cell).html()));
+				
+				if (color) {
+					$(cell).css("background-color", color);	
+				}
+			});	
+		}
+	}
+	
+	function findBinForValue(value) {
+		for (var i = 0; i < colorBins.length; i += 1) {
+			if (colorBins[i].min < value && value <= colorBins[i].max) {
+				return colorBins[i].color;
+			}
+		}
+		return
 	}
 	
 	function addData (data) {
@@ -96,12 +173,11 @@ DATATABLEWIDGET.DataController = function () {
 			if (data.columns[i].type !== "associatedData") {
 				// This works because setting bVisible to undefined is equivalent to true not false
 				columns.push({"sTitle" : data.columns[i].title, "bVisible": data.columns[i].visible});		
-				$("#columns-selector").append("<label class='radio'><input type='radio' name='column-options' value='" + data.columns[i].title + "'>" + data.columns[i].title + "</label>")
 			} else {
 				// Puts an untitled column in the first spot where the plus/minus icon will go
 				columns.unshift({"sTitle" : ""});
 				handleAssociatedData = true;
-				// +1 because we are going to shift it below
+				
 				associatedDataTableColumn = i;
 			}
 		}
@@ -133,11 +209,11 @@ DATATABLEWIDGET.DataController = function () {
         });
 
 		if (data.style) {
-			$("table").attr("style", data.style);
+			$("#datatable").attr("style", data.style);
 		}
 		
 		initializeSubTable(associatedDataTableColumn);
-		initializeContextMenu();
+		initializeContextMenu(data, associatedDataTableColumn);
 	}
 	
 	initialize();
